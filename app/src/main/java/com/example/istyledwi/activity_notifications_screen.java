ab2ad70a25c2d186be75125538f78e50f2f87c4c
Meifanny Azri Shafira <meifannyshafira@gmail.com>
2021-10-30 19:46:51 +0700

    Semangat Anggi

---
+++ app/src/main/java/com/example/istyledwi/activity_notifications_screen.java
@@ -0,0 +1,13 @@
+package com.example.istyledwi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_notifications_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notifications_screen);
+    }
+}
---
+++ app/src/main/java/com/example/istyledwi/activity_saved_screen_2.java
@@ -0,0 +1,50 @@
+package com.example.istyledwi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class activity_saved_screen_2 extends AppCompatActivity {
+
+    int[] image =new int[]{
+            R.drawable.saved_foto_gaun, R.drawable.saved_foto_diet
+    };
+
+    String[] headline = new String[]{
+            "Inspirasi Gaun Pengantin", "Tips Kurus Dalam Seminggu"
+    };
+    String[] desc = new String[]{
+            "Upload by belle","Upload by ucup "
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saved_screen_2);
+
+        ListView listView = findViewById(R.id.list_view);
+
+        List<HashMap<String,String>> list = new ArrayList<>();
+
+        for(int i=0; i<2; i++){
+            HashMap<String,String> hashMap = new HashMap<>();
+            hashMap.put("image",Integer.toString(image[i]));
+            hashMap.put("headline", headline[i]);
+            hashMap.put("desc", desc[i]);
+            list.add(hashMap);
+
+        }
+        String[] from= {"image","headline","desc"};
+        int[] to = {R.id.imageView,R.id.textViewHeadline,R.id.description};
+
+        SimpleAdapter simpleAdapter = new SimpleAdapter(getBaseContext(), list, R.layout.activity_saved_screen_2, from, to);
+        listView.setAdapter(simpleAdapter);
+
+
+    }
+}
